1a268348a086d337dd93704f352be9cf84bd448f
StvanLeo <estebanoleas@gmail.com>
2026-07-30 16:23:34 -0500

    Basic shopping app with Spring Boot 4.1.0, Java 21, Spring Data JPA, Java annotation Lombok, PostgreSQL and docker

---
+++ src/main/java/com/demo/bookstorebatch/BookstoreBatchApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bookstorebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/bookstorebatch/BookstoreBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bookstorebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
