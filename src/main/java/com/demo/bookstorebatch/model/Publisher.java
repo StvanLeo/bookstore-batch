51fad71b581ab750e810eac13b280893e3987726
StvanLeo <estebanoleas@gmail.com>
2026-09-08 08:37:47 -0500

    Modify Publisher entity to use GeneratitonType.Identity

--- src/main/java/com/demo/bookstorebatch/model/Publisher.java
+++ src/main/java/com/demo/bookstorebatch/model/Publisher.java
@@ -1,16 +1,14 @@
 package com.demo.bookstorebatch.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "publishers")
 @Data
 public class Publisher {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String address;
